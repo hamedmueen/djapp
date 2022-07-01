85fb3fd092d134c1a976222f626f5fd561c5e0a4
hamedmueen <64982261+hamedmueen@users.noreply.github.com>
2022-06-30 21:03:51 -0400

    Update settings.py

--- djapp/djapp/settings.py
+++ djapp/djapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#wgs#uog=7jkcxzsp8wockmddf@+)kut2_eowbb66!$zakh2p%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['34.234.78.36']
 
 
 # Application definition
